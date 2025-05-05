91ab410b5cea4fc1e1a45fc95c23534881d8867b
Erik <erik.boinski@posteo.de>
2025-05-05 12:43:43 +0200

    initialize project structure with Maven and Spring Boot setup

---
+++ src/main/java/de/albbw/smartbooks/SmartbooksApplication.java
@@ -0,0 +1,13 @@
+package de.albbw.smartbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartbooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/albbw/smartbooks/SmartbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package de.albbw.smartbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
